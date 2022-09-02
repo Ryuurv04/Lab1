175250f55b25e4b97540e9a10be8cb3afc2d1ecf
Ranses Vergara <106293764+Ryuurv04@users.noreply.github.com>
2022-09-02 03:21:19 -0500

    Add files via upload

---
+++ lab17.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laboratorio 1.7</title>
+</head>
+<body>
+    <?php
+      $x=24;
+      $p1=3.1416;
+      $animal="conejo";
+      $saludos="Hola caracola";
+      echo $x, "<br>", $p1, "<br>", $animal, "<br>", $saludos;
+
+    
+    ?>
+</body>
+</html>
